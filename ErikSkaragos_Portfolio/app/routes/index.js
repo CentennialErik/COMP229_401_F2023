d7cf06bf274907827bbc9e26f004ab56ee72368b
Erik <eskarago@my.centennialcollege.ca>
2023-09-20 15:45:19 -0400

    set up folder syste, added extra modules, set up html for multiple pages

---
+++ ErikSkaragos_Portfolio/app/routes/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  // Render home page
+  res.render('home', {title: 'Home' });
+});
+
+router.get('/about', (req, res, next) => {
+  // Render about page
+  res.render('about', {title: 'About Me' });
+});
+
+router.get('/projects', (req, res, next) => {
+    //Render projects page
+    res.render('projects', {title: 'Projects'});
+});
+
+router.get('/services', (req, res, next) => {
+    //Render services page
+    res.render('services', {title: 'Services'});
+});
+
+router.get('/contact', (req, res, next) => {
+    //Render contact page
+    res.render('contact', {title: 'Contact'});
+});
+
+module.exports = router;
