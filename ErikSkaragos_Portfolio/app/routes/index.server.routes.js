506e3e0546556983ee08e8a90c8e6cddc8929a23
Erik <eskarago@my.centennialcollege.ca>
2023-09-24 18:10:53 -0400

    Completed home.ejs and about.ejs. Also set up a css stylesheet.

--- ErikSkaragos_Portfolio/app/routes/index.server.routes.js
+++ ErikSkaragos_Portfolio/app/routes/index.server.routes.js
@@ -1,4 +1,23 @@
 module.exports = function(app) {
     var index = require('../controllers/index.server.controller');
+  
+    // Route for the Home page
     app.get('/', index.render);
-};
+    // Route for the About page
+    app.get('/about', function(req, res) {
+      res.render('about');
+    });
+    // Route for the Projects page
+    app.get('/projects', function(req, res) {
+      res.render('projects');
+    });
+    // Route for the Services page
+    app.get('/services', function(req, res) {
+      res.render('services');
+    });
+    // Route for the Contact page
+    app.get('/contact', function(req, res) {
+      res.render('contact');
+    });
+  };
+  
--- ErikSkaragos_Portfolio/config/express.js
+++ ErikSkaragos_Portfolio/config/express.js
@@ -2,7 +2,8 @@ var express = require('express'),
     morgan = require("morgan"),
     compress = require("compression"),
     bodyParser = require("body-parser"),
-    methodOverride = require("method-override");
+    methodOverride = require("method-override"),
+    path = require('path');
 
 module.exports = function() {
     var app = express();
@@ -18,9 +19,12 @@ module.exports = function() {
             extended: true,
         })
     );
-
     app.use(bodyParser.json());
     app.use(methodOverride());
+
+    app.use(express.static(path.join(__dirname, '../public')));
+    app.set('view engine', 'ejs');
+    app.set('views', path.join(__dirname, '../views'));
 
     require('../app/routes/index.server.routes.js')(app);
     return app;
