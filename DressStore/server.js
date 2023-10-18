d34450e1cf234c5c04e31fb33c6f545a7af4ba6d
Erik <eskarago@my.centennialcollege.ca>
2023-10-18 13:04:38 -0400

    minor changes to server.js

--- DressStore/server.js
+++ DressStore/server.js
@@ -1,10 +1,18 @@
-/*ErikSkaragos_Assignment2, Erik Skaragos, 301344796, October 18,2023. */
-process.env.NDOE_ENV = process.env.NODE_ENV || 'development'
+/*ErikSkaragos_Assignment2, Erik Skaragos, 301344796, October 18, 2023. */
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('./config/express');
-
 var app = express();
-app.listen(3000);
-module.exports = app;
 
-console.log('Server running at http://localhost:3000/');
+app.use('/api/products', require('./app/routes/productRoutes'));
+app.use('/api/categories', require('./app/routes/categoriesRoutes'));
+
+app.get('/', (req, res) => {
+  res.send('{"message":"Welcome to DressStore application."}');
+});
+
+app.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
+
+module.exports = app;
